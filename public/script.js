7c9370745f6936f58947deac95bdacd49580f668
Gayathri Masetty <gayathrimasetty@gayathri.local>
2021-07-12 11:40:12 +0530

    startpage

--- public/script.js
+++ public/script.js
@@ -290,40 +290,153 @@ function leaves(){
 
 
 
-// function loadAllEmoji() {
-//     const loadAllEmoji =() =>{
-//         var popup=document.getElementById("rightpage");
-//         var left = document.getElementById("leftpage");
-//         if(showDiv.val == "yes"){
-//             showDiv.style.cssText="color:white";
-//             popup.style.cssText = "display:none;flex:0";
-//             left.style.cssText="flex:1";
-//             showDiv.val = "no";
-//          }
-//          else{
-//             showDiv.style.cssText="color:khaki";
-//             popup.style.cssText="display:flex;flex:0.2";
-//             left.style.cssText="flex:0.8";
-//             showDiv.val = "yes"; 
-            
-//          }
-//      }
-//     var emoji = '';
-//     for (var i = 128512; i <= 128566; i++) {
-//         emoji += `<a href="#" style="font-size: 22px;" onclick="getEmoji(this)">&#${i};</a>`;
-//     }
 
-//     document.getElementById('sicons').innerHTML = emoji;
+
+
+
+
+
+
+
+
+
+'use strict';
+
+let mediaRecorder;
+let recordedBlobs;
+
+const codecPreferences = document.querySelector('#codecPreferences');
+
+// const errorMsgElement = document.querySelector('span#errorMsg');
+// const recordedVideo = document.querySelector('video#recorded');
+const recordButton = document.querySelector('button#record');
+recordButton.addEventListener('click', () => {
+  if (recordButton.textContent === 'Start Recording') {
+    startRecording();
+  } else {
+    stopRecording();
+    recordButton.textContent = 'Start Recording';
+    // playButton.disabled = false;
+    downloadButton.disabled = false;
+    codecPreferences.disabled = false;
+  }
+});
+
+// const playButton = document.querySelector('button#play');
+// playButton.addEventListener('click', () => {
+//   const mimeType = codecPreferences.options[codecPreferences.selectedIndex].value.split(';', 1)[0];
+//   const superBuffer = new Blob(recordedBlobs, {type: mimeType});
+//   recordedVideo.src = null;
+//   recordedVideo.srcObject = null;
+//   recordedVideo.src = window.URL.createObjectURL(superBuffer);
+// //   recordedVideo.controls = true;
+// //   recordedVideo.play();
+// });
+
+const downloadButton = document.querySelector('button#download');
+downloadButton.addEventListener('click', () => {
+  const blob = new Blob(recordedBlobs, {type: 'video/webm'});
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.style.display = 'none';
+  a.href = url;
+  a.download = 'test.webm';
+  document.body.appendChild(a);
+  a.click();
+  setTimeout(() => {
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }, 100);
+});
+
+// function handleDataAvailable(event) {
+//   console.log('handleDataAvailable', event);
+//   if (event.data && event.data.size > 0) {
+//     recordedBlobs.push(event.data);
+//   }
 // }
 
-// function showEmojiPanel() {
-//     document.getElementById('emoji').removeAttribute('style');
+// function getSupportedMimeTypes() {
+//   const possibleTypes = [
+//     'video/webm;codecs=vp9,opus',
+//     'video/webm;codecs=vp8,opus',
+//     'video/webm;codecs=h264,opus',
+//     'video/mp4;codecs=h264,aac',
+//   ];
+//   return possibleTypes.filter(mimeType => {
+//     return MediaRecorder.isTypeSupported(mimeType);
+//   });
 // }
 
-// function hideEmojiPanel() {
-//     document.getElementById('emoji').setAttribute('style', 'display:none;');
-// }
+function startRecording() {
+  recordedBlobs = [];
+  const mimeType = codecPreferences.options[codecPreferences.selectedIndex].value;
+  const options = {mimeType};
 
-// function getEmoji(control) {
-//     document.getElementById('txtMessage').value += control.innerHTML;
-// }
+  try {
+    mediaRecorder = new MediaRecorder(window.stream, options);
+  } catch (e) {
+    console.error('Exception while creating MediaRecorder:', e);
+    errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
+    return;
+  }
+
+  console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+  recordButton.textContent = 'Stop Recording';
+  playButton.disabled = true;
+  downloadButton.disabled = true;
+  codecPreferences.disabled = true;
+  mediaRecorder.onstop = (event) => {
+    console.log('Recorder stopped: ', event);
+    console.log('Recorded Blobs: ', recordedBlobs);
+  };
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.start();
+  console.log('MediaRecorder started', mediaRecorder);
+}
+
+function stopRecording() {
+  mediaRecorder.stop();
+}
+
+function handleSuccess(stream) {
+  recordButton.disabled = false;
+  console.log('getUserMedia() got stream:', stream);
+  window.stream = stream;
+
+  const gumVideo = document.querySelector('video#gum');
+  gumVideo.srcObject = stream;
+
+  getSupportedMimeTypes().forEach(mimeType => {
+    const option = document.createElement('option');
+    option.value = mimeType;
+    option.innerText = option.value;
+    codecPreferences.appendChild(option);
+  });
+  codecPreferences.disabled = false;
+}
+
+async function init(constraints) {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    console.error('navigator.getUserMedia error:', e);
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
+}
+
+document.querySelector('button#start').addEventListener('click', async () => {
+  document.querySelector('button#start').disabled = true;
+  const hasEchoCancellation = document.querySelector('#echoCancellation').checked;
+  const constraints = {
+    audio: {
+      echoCancellation: {exact: hasEchoCancellation}
+    },
+    video: {
+      width: 1280, height: 720
+    }
+  };
+  console.log('Using media constraints:', constraints);
+  await init(constraints);
+});
